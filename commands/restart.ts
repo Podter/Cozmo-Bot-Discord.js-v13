38118b5d1bf68ece7ea831670dd15dbf1fd36aaa
Podter <podter045@gmail.com>
2022-04-02 22:46:31 +0700

    commit

--- commands/restart.ts
+++ commands/restart.ts
@@ -8,8 +8,9 @@ export default {
     hidden: true,
     ownerOnly: true,
     testOnly: true,
-    callback: ({ interaction }) => {
+    callback: async ({ interaction }) => {
         interaction.reply("https://cdn.discordapp.com/attachments/959692896720797736/959830380549144586/a_mimir.mp4")
+        await new Promise((resolve) => setTimeout(resolve, 2000))
         process.exit(0)
     },
 } as ICommand
