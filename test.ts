17be7cb9bd4b2b031103aa6623e0c5800955ab40
Podter <podter045@gmail.com>
2022-04-09 11:46:55 +0700

    commit

--- test.ts
+++
@@ -1,8 +0,0 @@
-import { Lyrics } from "@discord-player/extractor";
-import dotenv from "dotenv";
-dotenv.config();
-const lyricsClient = Lyrics.init(process.env.GENIUS_ACCESS_TOKEN);
-
-lyricsClient.search("asdasdasdasdasdasjdfkhkasldfj")
-    .then(x => console.log(x))
-    .catch(console.error);
