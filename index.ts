f9d6893104446c15fd9731e8757e35e4e778eee9
Podter <48936015+Podter@users.noreply.github.com>
2022-04-30 18:03:57 +0700

    export client

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ import express from "express"
 import apiRouter from "./modules/api/api";
 const app = express()
 
-const client = new Discord.Client({
+export const client = new Discord.Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MEMBERS]
 });
 
